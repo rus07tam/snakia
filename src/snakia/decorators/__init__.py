694dbf99be47879e6ef4cf35271b0a3b6effb54e
rus07tam <rus07tam@gmail.com>
2025-11-23 09:20:45 +0000

    feat: add inject_decorator, hook_decorator and replace_decorator

--- src/snakia/decorators/__init__.py
+++ src/snakia/decorators/__init__.py
@@ -2,17 +2,21 @@ from .inject_after import after_hook, inject_after
 from .inject_before import before_hook, inject_before
 from .inject_const import inject_const
 from .inject_replace import inject_replace, replace_hook
+from .meta_decorators import hook_decorator, inject_decorator, replace_decorator
 from .pass_exceptions import pass_exceptions
 from .singleton import singleton
 
 __all__ = [
-    "inject_replace",
-    "replace_hook",
-    "inject_after",
     "after_hook",
-    "inject_before",
     "before_hook",
+    "inject_after",
+    "inject_before",
     "inject_const",
+    "inject_decorator",
+    "inject_replace",
+    "hook_decorator",
     "pass_exceptions",
+    "replace_decorator",
+    "replace_hook",
     "singleton",
 ]
