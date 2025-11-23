c879b22f43ef1a4ba3a5e2116e52529a6b4e7901
rus07tam <rus07tam@gmail.com>
2025-11-23 10:14:46 +0000

    feat: add side and side_func – clean alternative to "or print()" in lambdas

--- src/snakia/utils/__init__.py
+++ src/snakia/utils/__init__.py
@@ -2,6 +2,7 @@ from .attr import get_attrs, get_or_set_attr
 from .frame import frame
 from .inherit import inherit
 from .nolock import nolock
+from .side import side, side_func
 from .this import this
 from .throw import throw
 from .to_async import to_async
@@ -12,6 +13,8 @@ __all__ = [
     "frame",
     "inherit",
     "nolock",
+    "side",
+    "side_func",
     "this",
     "throw",
     "to_async",
